0df6223361cec8b234366a37fb6df061ecf6d77e
myrfik <lera.ogay.01@bk.ru>
2024-04-10 14:00:39 +0600

    KS-3-1 greetings

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName: String = "Виолетта"
+    var greetings: String = "Добрый день"
+
+    println("$greetings, $userName!")
+
+    greetings = "Добрый вечер"
+
+    println("$greetings, $userName!")
+}
