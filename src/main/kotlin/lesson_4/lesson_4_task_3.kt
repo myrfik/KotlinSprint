40ce3fe5dd24de157120b7d9aef331fd2b98f633
myrfik <lera.ogay.01@bk.ru>
2024-04-14 18:47:37 +0600

    KS-4-3 refactor

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,13 +1,18 @@
 package org.example.lesson_4
 
 fun main() {
-    val presenceOfSunnyWeather: Boolean = true
+    val weatherToday: Boolean = true
     val tentOpen: Boolean = true
-    val airHumidity: Int = 20
-    val season: String = "зима"
+    val airHumidityNow: Int = 20
+    val seasonNow: String = "зима"
 
     val favorableConditionsForLegumes =
-        (presenceOfSunnyWeather && tentOpen && (airHumidity == 20) && (season != "зима"))
+        ((weatherToday == IS_SUNNY) && (tentOpen == TENT_NOW) && (airHumidityNow == AIR_HUMIDITY) && (seasonNow != SEASON))
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditionsForLegumes")
 }
+
+const val IS_SUNNY: Boolean = true
+const val TENT_NOW: Boolean = true
+const val AIR_HUMIDITY: Int = 20
+const val SEASON: String = "зима"
