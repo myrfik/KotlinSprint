d0ac1c80049f363c46bfaa1623a36bf04da545f7
myrfik <lera.ogay.01@bk.ru>
2024-04-14 19:17:19 +0600

    KS-4-5 refactor

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -3,27 +3,29 @@ package org.example.lesson_4
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.`in`)
 
     println("Присутствуют ли повреждения корпуса? (true/false): ")
-    val hullDamages = scanner.nextBoolean()
+    val hullDamages = readln().toBoolean()
 
     println("Введите текущий состав экипажа: ")
-    val crewComposition = scanner.nextInt()
+    val crewComposition = readln().toInt()
 
     println("Введите количество ящиков с провизией на борту: ")
-    val amountOfProvisions = scanner.nextInt()
+    val amountOfProvisions = readln().toInt()
 
     println("Благоприятные ли условия для отправления корабля в плавание? (true/false): ")
-    val weatherConditions = scanner.nextBoolean()
+    val weatherConditions = readln().toBoolean()
 
-    val favorableConditions = (!hullDamages && ((crewComposition > MIN_CREW_SIZE)
+    val favorableConditions = ((hullDamages == IS_DAMAGE) && ((crewComposition > MIN_CREW_SIZE)
             && (crewComposition <= MAX_CREW_SIZE)) && (amountOfProvisions > MIN_AMOUNT_PROVISION) &&
-            (weatherConditions || !weatherConditions)) || (hullDamages && (crewComposition == MAX_CREW_SIZE)
-            && (amountOfProvisions >= MIN_AMOUNT_PROVISION))
+            ((weatherConditions == WEATHER) || (weatherConditions != WEATHER))) || ((hullDamages != IS_DAMAGE) &&
+            (crewComposition == MAX_CREW_SIZE) && (amountOfProvisions >= MIN_AMOUNT_PROVISION))
 
     println("Корабль может отправиться в плавание: $favorableConditions")
 }
+
+const val IS_DAMAGE = false
 const val MIN_CREW_SIZE = 55
 const val MAX_CREW_SIZE = 70
 const val MIN_AMOUNT_PROVISION = 50
+const val WEATHER = true
