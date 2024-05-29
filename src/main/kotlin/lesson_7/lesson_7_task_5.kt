38447a406345bd2cd3d5779ccf0c3924bce87027
myrfik <lera.ogay.01@bk.ru>
2024-05-29 21:33:19 +0600

    KS-7-5 refactor

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -1,26 +1,25 @@
 package org.example.lesson_7
 
 fun main() {
-    val digit = 0..9
+    val digit = '0'..'9'
     val letter = 'a'..'z'
     val capitalLetter = 'A'..'Z'
-    var password = ""
+    val chars = digit + letter + capitalLetter
 
     println("Введите длину пароля (минимум 6 символов): ")
     val passwordLength = readln().toInt()
 
+    val charForPassword = mutableListOf<Char>()
+    charForPassword.add(digit.random())
+    charForPassword.add(letter.random())
+    charForPassword.add(capitalLetter.random())
+
     for (i in 0 until passwordLength) {
-        if (i % 2 == 0) {
-            password += digit.random()
-        } else if (i % 3 == 0) {
-            password += letter.random()
-        } else {
-            password += capitalLetter.random()
-        }
+        charForPassword.add(chars.random())
     }
 
-    val shuffledPassword = password.toList().shuffled()
-    val resultPassword = shuffledPassword.joinToString("")
+    val shuffledPassword = charForPassword.shuffled()
+    val password = shuffledPassword.joinToString("")
 
-    println("Ваш пароль: $resultPassword")
+    println("Ваш пароль: $password")
 }
