f115772fb83b7f13df61b935d1b31b1dc90e4f4c
myrfik <lera.ogay.01@bk.ru>
2024-09-24 20:23:43 +0600

    KS-8-1 error correction

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -2,11 +2,7 @@ package org.example.lesson_8
 
 fun main() {
     val viewsPerDay = arrayOf(199, 223, 158, 654, 521, 343, 551)
-    var totalViews = 0
-
-    for (i in viewsPerDay) {
-        totalViews += i
-    }
+    val totalViews = viewsPerDay.sum()
     println("Общее количество просмотров за неделю составило: $totalViews")
 }
 
