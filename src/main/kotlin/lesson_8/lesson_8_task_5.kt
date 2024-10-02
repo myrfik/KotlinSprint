43a47f2709ec2b62a29db36e735e6a868bba1041
myrfik <lera.ogay.01@bk.ru>
2024-10-02 20:42:13 +0600

    KS-8-5 refactor

--- src/main/kotlin/lesson_8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -2,18 +2,18 @@ package org.example.lesson_8
 
 fun main() {
     println("Введите количество планируемых ингредиентов: ")
-    val quantityOfIngredients = readln().toInt()
+    val quantityOfIngredients = readln().toIntOrNull() ?: return
 
-    var ingredients = emptyArray<String>()
+    var ingredients = arrayOfNulls<String>(quantityOfIngredients)
 
 
-    for (i in 1..quantityOfIngredients) {
+    for (i in ingredients) {
         println("Введите новый ингридиент: ")
         var introducedIngredient = readln()
         ingredients = ingredients.plus(introducedIngredient)
     }
 
-    val result = ingredients.joinToString(separator = ", ")
+    val result = ingredients.filterNotNull().joinToString(separator = ", ")
     println("Список ингридиентов: $result")
 
 }
